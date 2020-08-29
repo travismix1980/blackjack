fb3f322835f22e6710c68da65f4c8b8434b92d81
Travis Mix <travismix1980@gmail.com>
2020-08-29 16:12:42 -0700

    Added a card object and deckOfCards object that can make a deck and shuffle that deck

---
+++ app.js
@@ -0,0 +1,30 @@
+const cards = {
+	deck: [],
+	suits: [ 'clubs', 'hearts', 'spades', 'diamonds' ],
+	values: [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ],
+	makeDeck() {
+		const { values, suits, deck } = this;
+		for (let value of values) {
+			for (let suit of suits) {
+				deck.push({ value, suit });
+			}
+		}
+		return deck;
+	}
+};
+
+const deckOfCards = {
+	deck: [ ...cards.makeDeck() ],
+	shuffleDeck() {
+		const { deck } = this;
+		for (let i = deck.length - 1; i > 0; i--) {
+			let randomPos = Math.floor(Math.random() * deck.length);
+			let temp = deck[i];
+			deck[i] = deck[randomPos];
+			deck[randomPos] = temp;
+		}
+    },
+    
+};
+
+
